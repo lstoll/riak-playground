e04d3b1bb349b423e67373b725056515614e8679
Lincoln Stoll <lstoll@lstoll.net>
2010-02-17 23:46:11 +0100

    Load 20k basic docs, with a link to it's previous tagged parent.

---
+++ 4_perf_tests/1_load_20k.rb
@@ -0,0 +1,17 @@
+require 'riak'
+
+# Connect and get a bucket instance
+client = Riak::Client.new
+bucket = client.bucket("test4")
+
+prev = nil
+
+(1..20000).each do |i|
+  puts "Saving item #{i}" if i % 10 == 0
+  o = Riak::RObject.new(bucket, i)
+  o.content_type = "application/javascript"
+  o.data = "{'item': 'one'}"
+  o.links << prev.to_link("parent") if prev
+  o.store
+  prev = o
+end
