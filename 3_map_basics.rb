71cd31894d65dec83aefbf96b2881015dd742bdf
Lincoln Stoll <lstoll@lstoll.net>
2010-02-17 21:36:09 +0100

    Some basic map stuff

---
+++ 3_map_basics.rb
@@ -0,0 +1,65 @@
+require 'riak'
+require 'uuid'
+
+def gen_id
+  UUID.create.to_s.gsub('-','')  
+end
+
+# Connect and get a bucket instance
+client = Riak::Client.new
+bucket = client.bucket("test3")
+
+# Ensure bucket is empty. When bugfixed, pass block direct to bucket
+bucket.keys.each {|k| bucket.get(k).delete}
+
+# Create and save a simple JSON object
+new_one = Riak::RObject.new(bucket, "item1")
+new_one.content_type = "application/javascript" # You must set the content type.
+new_one.data = '{"item": "one"}'
+new_one.store
+
+# Create and save a second JSON object, linking to the first
+new_two = Riak::RObject.new(bucket, "item2")
+new_two.content_type = "application/javascript" # You must set the content type.
+new_two.data = '{"item": "two"}'
+# Linking. Must be a better way to create the URL?. Link is tagged parent
+new_two.store
+
+
+puts "*** Simple Query ***"
+p Riak::MapReduce.new(client).
+                add(bucket.name). # run on all items in bucket
+                # next line is same as map("Riak.mapValuesJson", :keep => true)
+                map("function(v) {
+                      // Load the items data, and parse it
+                      obj = JSON.parse(v.values[0].data);
+                      if (obj.item && obj.item == 'two') {
+                        return [obj];
+                      }
+                      else {
+                        return []; 
+                      }
+                    }",
+                    :keep => true).run # Keep make sure we get the output
+
+# This example doesn't achieve anything more than above, but shows how to chain maps
+puts "*** Multi Map Phase Query ***"
+mr = Riak::MapReduce.new(client).
+                add(bucket.name). # run on all items in bucket
+                map("function(v) {
+                      obj = JSON.parse(v.values[0].data);
+                      if (obj.item && obj.item == 'two') {
+                        // Need to return bucket/key pairs if the target is another map
+                        return [[v.bucket, v.key]];
+                      }
+                      else {
+                        return []; 
+                      }
+                      
+                    }").
+                map("function(v) {
+                      return [JSON.parse(v.values[0].data)];
+                    }", # Just return the JSON representation
+                    :keep => true)
+
+p mr.run
--- TIPS.md
+++ TIPS.md
@@ -1,3 +1,9 @@
+= Gotchas
+
+Content type for json must be application/javascript, not application/json . Also it must use double quotes for strings, single seemingly will silently fail
+
+If you want to have multiple map phases, the first map phase should return [bucket,key] pairs for the next phase
+
 = Some hints
 
 == Submit a query
@@ -5,3 +11,4 @@
     curl -X POST -H "content-type: application/json" http://localhost:8098/mapred --data @-
 
 Then enter the query json, return, ^D
+
